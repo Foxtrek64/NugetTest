efce2aa0439f30223a07947ba8d01878cd6adbe9
Devin Duanne <dduanne@tafs.com>
2019-12-31 12:39:26 -0600

    Add nuget test

---
+++ NugetTest/MathLibrary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NugetTest
+{
+    public class MathLibrary
+    {
+        public int Add(int left, int right)
+            => left + right;
+
+        public int Subtract(int left, int right)
+            => left - right;
+
+        public int Multiply(int left, int right)
+            => left * right;
+
+        public int Divide(int numerator, int denominator)
+            => numerator / denominator;
+    }
+}
